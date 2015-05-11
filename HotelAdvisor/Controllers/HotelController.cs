0f5523200dce9910902b7f43a2997250717c82c9
n.babin <n.babin@levi9.com>
2015-05-11 21:29:40 +0200

    Inital commit

---
+++ HotelAdvisor/Controllers/HotelController.cs
@@ -0,0 +1,107 @@
+﻿using HotelAdvisor.Managers;
+using HotelAdvisor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelAdvisor.Controllers
+{
+    public class HotelController : Controller
+    {
+        // GET: Hotel
+        public ActionResult Index()
+        {
+            HotelManager manager = new HotelManager();
+            List<HotelDetailsViewModel> model = manager.GetHotels();
+
+            return View(model);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create(HotelViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                HotelManager manager = new HotelManager();
+                Hotel hotel = new Hotel();
+                hotel.Name = model.Name;
+                hotel.Description = model.Description;
+                hotel.City = model.City;
+                hotel.Address = model.Address;
+                hotel.HouseNumber = model.HouseNumber;
+                hotel.IsActive = model.IsActive;
+                hotel.Image = model.Image;
+                manager.Create(hotel);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int id)
+        {
+            HotelManager manager = new HotelManager();
+            Hotel model = manager.Find(id);
+
+            HotelViewModel hotel = new HotelViewModel();
+            hotel.Id = model.Id;
+            hotel.Name = model.Name;
+            hotel.Description = model.Description;
+            hotel.City = model.City;
+            hotel.Address = model.Address;
+            hotel.HouseNumber = model.HouseNumber;
+            hotel.IsActive = model.IsActive;
+
+            return View(hotel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(HotelViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                HotelManager manager = new HotelManager();
+                Hotel hotel = new Hotel();
+                hotel.Id = model.Id;
+                hotel.Name = model.Name;
+                hotel.Description = model.Description;
+                hotel.City = model.City;
+                hotel.Address = model.Address;
+                hotel.HouseNumber = model.HouseNumber;
+                hotel.IsActive = model.IsActive;
+                hotel.Image = hotel.Image;
+
+                manager.Edit(hotel);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            HotelManager manager = new HotelManager();
+            HotelDetailsViewModel model = manager.GetHotelDetails(id);
+            return View(model);
+        }
+    }
+}
---
+++ HotelAdvisor/Models/HotelViewModel.cs
@@ -0,0 +1,36 @@
+﻿using HotelAdvisor.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace HotelAdvisor.Controllers
+{
+    public class HotelViewModel
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Display(Name = "House number")]
+        public int HouseNumber { get; set; }
+
+        public string City { get; set; }
+
+        public string Image { get; set; }
+
+        [Display(Name = "Active")]
+        public bool IsActive { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
